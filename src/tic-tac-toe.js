53ea284d5f860c828390af67ee09d0bab6feeefd
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2017-02-03 17:02:25 +0300

    start create game

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,52 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentPlayerSymbol = 0;
+    this.table = new Array();
 
+    for (let i = 0; i < 3; i += 1) {
+      this.table[i] = new Array();
+      for (let j = 0; j < 3; j += 1) {
+        this.table[i][j] = null;
+      }
+    }
+  }
+
+  getCurrentPlayerSymbol() {
+    return !this.currentPlayerSymbol ? 'x' : 'o';
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+
+    if (this.table[rowIndex][columnIndex] !== null) {
+      return;
     }
 
-    getCurrentPlayerSymbol() {
+    this.table[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
 
-    }
+    this.currentPlayerSymbol = !this.currentPlayerSymbol ? 1 : 0;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  isFinished() {
+    console.log('isFinished');
+  }
 
-    }
+  getWinner() {
+    console.log('getWinner');
 
-    isFinished() {
 
-    }
+  }
 
-    getWinner() {
+  noMoreTurns() {
+    console.log('noMoreTurns');
+  }
 
-    }
+  isDraw() {
+    console.log('isDraw');
+  }
 
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.table[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
