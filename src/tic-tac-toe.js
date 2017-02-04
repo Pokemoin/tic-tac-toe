fc6b5f8163f78c9d7b1d457682c385506c0167be
nikolaiverbitskii <professorsport13@gmail.com>
2017-02-04 13:21:24 +0300

    complete game

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,12 +1,15 @@
 class TicTacToe {
   constructor() {
     this.currentPlayerSymbol = 0;
-    this.table = new Array();
+    this.matrix = new Array();
+    this._isFinished = false;
+    this.countFreeFields = 0;
 
     for (let i = 0; i < 3; i += 1) {
-      this.table[i] = new Array();
+      this.matrix[i] = new Array();
       for (let j = 0; j < 3; j += 1) {
-        this.table[i][j] = null;
+        this.matrix[i][j] = null;
+        this.countFreeFields++;
       }
     }
   }
@@ -17,35 +20,82 @@ class TicTacToe {
 
   nextTurn(rowIndex, columnIndex) {
 
-    if (this.table[rowIndex][columnIndex] !== null) {
+    if (this.matrix[rowIndex][columnIndex] !== null) {
       return;
     }
 
-    this.table[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+    // set matrix cell value
+    this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
 
+    // decrease number of countTurns
+    this.countFreeFields--;
+
+    // check available turns
+    if (this.noMoreTurns()) {
+      this._isFinished = true;
+    }
+
+    // check winner
+    if (this.getWinner() !== null) {
+      this._isFinished = true;
+    }
+
+    // change currentPlayerSymbol
     this.currentPlayerSymbol = !this.currentPlayerSymbol ? 1 : 0;
   }
 
   isFinished() {
-    console.log('isFinished');
+    return this._isFinished;
   }
 
   getWinner() {
-    console.log('getWinner');
 
+    let winner = null,
+        horResult = false,
+        vertResult = false,
+        matrix = this.matrix;
 
+    // check horResult result
+    matrix.map(function (cell) {
+      if (cell[0] !== null && cell[0] == cell[1] && cell[0] == cell[2]) {
+        winner = cell[0];
+        horResult = true;
+      }
+    });
+
+    // check vertResult result
+    if (!horResult) {
+      matrix[0].forEach(function (cell, i) {
+        if (cell !== null && cell == matrix[1][i] && cell == matrix[2][i]) {
+          winner = cell;
+          vertResult = true;
+        }
+      });
+    }
+
+    // check diagResult result
+    if (!horResult && !vertResult) {
+      if (matrix[0][0] !== null && matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2]) {
+        winner = matrix[0][0];
+      }
+      if (matrix[0][2] !== null && matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0]) {
+        winner = matrix[0][2];
+      }
+    }
+
+    return winner;
   }
 
   noMoreTurns() {
-    console.log('noMoreTurns');
+    return this.countFreeFields <= 0 ? true : false;
   }
 
   isDraw() {
-    console.log('isDraw');
+    return (this.getWinner() == null && this.noMoreTurns()) ? true : false;
   }
 
   getFieldValue(rowIndex, colIndex) {
-    return this.table[rowIndex][colIndex];
+    return this.matrix[rowIndex][colIndex];
   }
 }
 
